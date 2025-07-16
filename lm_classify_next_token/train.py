6ead544ce8404cdb0ad91a4dcb2655f0926e7017
parth sarin <psarin@stanford.edu>
2025-07-16 11:28:14 -0700

    add data loading to training

--- lm_classify_next_token/train.py
+++ lm_classify_next_token/train.py
@@ -27,6 +27,12 @@ def parse_args():
     # train subcommand
     train_parser = subparsers.add_parser("train", help="Train a model.")
     train_parser.add_argument("--input_file", type=str, default="train_dataset.jsonl", help="Path to the training data file.")
+    train_parser.add_argument("--model", type=str, default="Qwen/Qwen2.5-7B-Instruct", help="Model to use for classification.")
+    train_parser.add_argument("--output_dir", type=str, default="checkpoints", help="Path to the output directory.")
+
+    train_parser.add_argument("--lora_r", type=int, default=8, help="Lora rank.")
+    train_parser.add_argument("--lora_alpha", type=int, default=16, help="Lora alpha.")
+    train_parser.add_argument("--lora_dropout", type=float, default=0.1, help="Lora dropout.")
 
     return parser.parse_args()
 
@@ -56,6 +62,53 @@ def create_dataset(args):
                 line = in_f.readline()
                 out_f.write(line)
 
+
+def train_model(args):
+    # load the peft model
+    model = AutoModelForCausalLM.from_pretrained(args.model, device_map = 'auto')
+    tokenizer = AutoTokenizer.from_pretrained(args.model)
+    peft_config = LoraConfig(
+        r = args.lora_r,
+        lora_alpha = args.lora_alpha,
+        lora_dropout = args.lora_dropout,
+        task_type = "CAUSAL_LM",
+        target_modules = ['q_proj', 'k_proj', 'v_proj', 'o_proj', 'gate_proj', 'up_proj', 'down_proj']
+    )
+    model = get_peft_model(model, peft_config)
+    model.print_trainable_parameters()
+    
+    # load the dataset
+    print(f'Loading dataset from {args.input_file}')
+    data = []
+    valid_rtgs = {x.lower() for x in CATEGORIES.values()} - {'text', 'other'}
+    rtg_to_number = {rtg: i for i, rtg in CATEGORIES.items() if rtg.lower() in valid_rtgs}
+    with open(args.input_file, "r") as f:
+        for line in f:
+            metadata = json.loads(line)
+            rtg = metadata.get('attributes.types.resourceTypeGeneral')
+            if not rtg:
+                continue
+            if rtg.lower() not in valid_rtgs:
+                continue
+
+            resource_desc = '\n'.join(
+                f'{k}: {v}'
+                for k, v in metadata.items()
+                if k != 'attributes.types.resourceTypeGeneral'
+            )
+
+            prompt = tokenizer.apply_chat_template([
+                {'role': 'system', 'content': SYSTEM_PROMPT},
+                {'role': 'user', 'content': resource_desc}
+            ], tokenize=False)
+            
+            data.append({
+                "prompt": prompt,
+                "label": rtg,
+                'completion': rtg_to_number[rtg]
+            })
+    print(f'Loaded {len(data)} examples')
+    breakpoint()
 
 def main():
     args = parse_args()
